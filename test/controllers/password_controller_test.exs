45264c99938b9b5109b4b38905d86ceea15bcb28
Jack Murphy <jmurphy.web@gmail.com>
2017-05-16 12:35:30 +0100

    #15 - [WIP] fix old password reset test

--- test/controllers/password_controller_test.exs
+++ test/controllers/password_controller_test.exs
@@ -27,11 +27,19 @@ defmodule Karma.PasswordControllerTest do
   end
 
   test "password :create with recognised email address", %{conn: conn} do
-    # NOTE should mock the email function
-    user = insert_user()
-    conn = post(conn, password_path(conn, :create), user: @valid_attrs)
-    assert redirected_to(conn) == session_path(conn, :new)
-    assert Phoenix.Controller.get_flash(conn, :info) =~ "A password reset email has been sent to #{user.email}"
+    with_mock Karma.Mailer, [deliver_later: fn(_) -> nil end] do
+      user = insert_user()
+      conn = post(conn, password_path(conn, :create), user: @valid_attrs)
+      # test response
+      assert redirected_to(conn) == session_path(conn, :new)
+      assert Phoenix.Controller.get_flash(conn, :info) =~ "A password reset email has been sent to #{user.email}"
+      # test Redis has key, which represents our email
+      {:ok, [key]} = RedisCli.query(["keys", "*"])
+      assert RedisCli.get(key) == {:ok, "test@test.com"}
+      # test key is set to expire in 5 mins
+      {:ok, ttl} = RedisCli.query(["ttl", key])
+      assert ttl == 300
+    end
   end
 
   test "password :create with fake email address", %{conn: conn} do
