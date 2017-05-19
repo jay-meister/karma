588324bd4cd21d42e368f1c58382f102567ce777
Jack Murphy <jmurphy.web@gmail.com>
2017-05-19 19:03:55 +0100

    #88 - run validation before doing calculations

--- web/controllers/offer_controller.ex
+++ web/controllers/offer_controller.ex
@@ -55,41 +55,6 @@ defmodule Karma.OfferController do
     sixth_day_fee_multiplier = String.to_float(sixth_day_fee_multiplier)
     seventh_day_fee_multiplier = String.to_float(seventh_day_fee_multiplier)
 
-    working_week =
-      case working_week do
-        "" -> ""
-        _ww -> String.to_float(working_week)
-      end
-    fee_per_day_inc_holiday =
-      case fee_per_day_inc_holiday do
-         "" -> ""
-         _fpdih -> String.to_integer(fee_per_day_inc_holiday)
-      end
-
-    fee_per_day_exc_holiday = calc_fee_per_day_exc_holiday(fee_per_day_inc_holiday, project.holiday_rate)
-    holiday_pay_per_day = calc_holiday_pay_per_day(fee_per_day_inc_holiday, fee_per_day_exc_holiday)
-    fee_per_week_inc_holiday = calc_fee_per_week_inc_holiday(fee_per_day_inc_holiday, working_week)
-    fee_per_week_exc_holiday = calc_fee_per_week_exc_holiday(fee_per_week_inc_holiday, project.holiday_rate)
-    holiday_pay_per_week = calc_holiday_pay_per_week(fee_per_week_inc_holiday, fee_per_week_exc_holiday)
-    contract_type = determine_contract_type(department, job_title)
-    sixth_day_fee_inc_holiday = calc_day_fee_inc_holidays(fee_per_day_inc_holiday, sixth_day_fee_multiplier)
-    sixth_day_fee_exc_holiday = calc_day_fee_exc_holidays(fee_per_day_exc_holiday, sixth_day_fee_multiplier)
-    seventh_day_fee_inc_holiday = calc_day_fee_inc_holidays(fee_per_day_inc_holiday, seventh_day_fee_multiplier)
-    seventh_day_fee_exc_holiday = calc_day_fee_exc_holidays(fee_per_day_exc_holiday, seventh_day_fee_multiplier)
-
-    offer_params = Map.put(offer_params, "fee_per_day_exc_holiday", fee_per_day_exc_holiday)
-    offer_params = Map.put(offer_params, "sixth_day_fee_multiplier", sixth_day_fee_multiplier)
-    offer_params = Map.put(offer_params, "seventh_day_fee_multiplier", seventh_day_fee_multiplier)
-    offer_params = Map.put(offer_params, "holiday_pay_per_day", holiday_pay_per_day)
-    offer_params = Map.put(offer_params, "fee_per_week_inc_holiday", fee_per_week_inc_holiday)
-    offer_params = Map.put(offer_params, "fee_per_week_exc_holiday", fee_per_week_exc_holiday)
-    offer_params = Map.put(offer_params, "holiday_pay_per_week", holiday_pay_per_week)
-    offer_params = Map.put_new(offer_params, "contract_type", contract_type)
-    offer_params = Map.put_new(offer_params, "sixth_day_fee_inc_holiday", sixth_day_fee_inc_holiday)
-    offer_params = Map.put_new(offer_params, "sixth_day_fee_exc_holiday", sixth_day_fee_exc_holiday)
-    offer_params = Map.put_new(offer_params, "seventh_day_fee_inc_holiday", seventh_day_fee_inc_holiday)
-    offer_params = Map.put_new(offer_params, "seventh_day_fee_exc_holiday", seventh_day_fee_exc_holiday)
-
     user = Repo.get_by(User, email: user_email)
 
     changeset = case user do
@@ -104,18 +69,62 @@ defmodule Karma.OfferController do
         |> Ecto.Changeset.put_assoc(:user, user)
     end
 
-    case Repo.insert(changeset) do
-      {:ok, offer} ->
-        # email function decides whether this is a registered user
-        Karma.Email.send_new_offer_email(conn, offer)
-        |> Karma.Mailer.deliver_later()
-        conn
-        |> put_flash(:info, "Offer sent to #{offer.target_email}")
-        |> redirect(to: project_offer_path(conn, :index, project_id))
-      {:error, changeset} ->
-        job_titles = Karma.Job.titles()
-        job_departments = Karma.Job.departments()
-        render(conn, "new.html", changeset: changeset, project_id: project_id, job_titles: job_titles, job_departments: job_departments)
+    if !changeset.valid? do
+      changeset = %{changeset | action: :insert} # manually set the action so errors are shown
+      job_titles = Karma.Job.titles()
+      job_departments = Karma.Job.departments()
+      render(conn, "new.html", changeset: changeset, project_id: project_id, job_titles: job_titles, job_departments: job_departments)
+    else
+
+      working_week =
+        case working_week do
+          "" -> ""
+          _ww -> String.to_float(working_week)
+        end
+      fee_per_day_inc_holiday =
+        case fee_per_day_inc_holiday do
+           "" -> ""
+           _fpdih -> String.to_integer(fee_per_day_inc_holiday)
+        end
+
+      fee_per_day_exc_holiday = calc_fee_per_day_exc_holiday(fee_per_day_inc_holiday, project.holiday_rate)
+      holiday_pay_per_day = calc_holiday_pay_per_day(fee_per_day_inc_holiday, fee_per_day_exc_holiday)
+      fee_per_week_inc_holiday = calc_fee_per_week_inc_holiday(fee_per_day_inc_holiday, working_week)
+      fee_per_week_exc_holiday = calc_fee_per_week_exc_holiday(fee_per_week_inc_holiday, project.holiday_rate)
+      holiday_pay_per_week = calc_holiday_pay_per_week(fee_per_week_inc_holiday, fee_per_week_exc_holiday)
+      contract_type = determine_contract_type(department, job_title)
+      sixth_day_fee_inc_holiday = calc_day_fee_inc_holidays(fee_per_day_inc_holiday, sixth_day_fee_multiplier)
+      sixth_day_fee_exc_holiday = calc_day_fee_exc_holidays(fee_per_day_exc_holiday, sixth_day_fee_multiplier)
+      seventh_day_fee_inc_holiday = calc_day_fee_inc_holidays(fee_per_day_inc_holiday, seventh_day_fee_multiplier)
+      seventh_day_fee_exc_holiday = calc_day_fee_exc_holidays(fee_per_day_exc_holiday, seventh_day_fee_multiplier)
+
+      offer_params = Map.put(offer_params, "fee_per_day_exc_holiday", fee_per_day_exc_holiday)
+      offer_params = Map.put(offer_params, "sixth_day_fee_multiplier", sixth_day_fee_multiplier)
+      offer_params = Map.put(offer_params, "seventh_day_fee_multiplier", seventh_day_fee_multiplier)
+      offer_params = Map.put(offer_params, "holiday_pay_per_day", holiday_pay_per_day)
+      offer_params = Map.put(offer_params, "fee_per_week_inc_holiday", fee_per_week_inc_holiday)
+      offer_params = Map.put(offer_params, "fee_per_week_exc_holiday", fee_per_week_exc_holiday)
+      offer_params = Map.put(offer_params, "holiday_pay_per_week", holiday_pay_per_week)
+      offer_params = Map.put_new(offer_params, "contract_type", contract_type)
+      offer_params = Map.put_new(offer_params, "sixth_day_fee_inc_holiday", sixth_day_fee_inc_holiday)
+      offer_params = Map.put_new(offer_params, "sixth_day_fee_exc_holiday", sixth_day_fee_exc_holiday)
+      offer_params = Map.put_new(offer_params, "seventh_day_fee_inc_holiday", seventh_day_fee_inc_holiday)
+      offer_params = Map.put_new(offer_params, "seventh_day_fee_exc_holiday", seventh_day_fee_exc_holiday)
+
+
+      case Repo.insert(changeset) do
+        {:ok, offer} ->
+          # email function decides whether this is a registered user
+          Karma.Email.send_new_offer_email(conn, offer)
+          |> Karma.Mailer.deliver_later()
+          conn
+          |> put_flash(:info, "Offer sent to #{offer.target_email}")
+          |> redirect(to: project_offer_path(conn, :index, project_id))
+        {:error, changeset} ->
+          job_titles = Karma.Job.titles()
+          job_departments = Karma.Job.departments()
+          render(conn, "new.html", changeset: changeset, project_id: project_id, job_titles: job_titles, job_departments: job_departments)
+      end
     end
   end
 
