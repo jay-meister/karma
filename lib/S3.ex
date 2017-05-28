8604f01f04c25968a615dc1f672677a936161c1a
Jack Murphy <jmurphy.web@gmail.com>
2017-05-28 11:44:50 +0100

    #168 - first attempt at using Task.async which is working, but has no effect on upload time

--- lib/S3.ex
+++ lib/S3.ex
@@ -3,25 +3,34 @@ defmodule Karma.S3 do
     keys
     # remove keys that haven't been uploaded
     |> Enum.filter(fn({file_key, _url_key}) -> Map.has_key?(params, file_key) end)
-    |> Enum.reduce(%{}, fn({file_key, url_key}, acc) ->
+    |> Enum.reduce([], fn({file_key, url_key}, acc) ->
         file = Map.get(params, file_key)
-        case upload(file) do
-          {:error, _msg} -> acc
-          {:ok, image_url} -> Map.put(acc, url_key, image_url)
-        end
+        task = Task.async(Karma.S3, :upload, [url_key, file])
+        [task | acc]
       end)
+    |> IO.inspect
+    |> Enum.map(&Task.await/1)
+    |> IO.inspect
+    |> Enum.filter(fn {res, _url_key, _url} -> res != :error end)
+    |> IO.inspect
+    |> Enum.map(fn { _res, url_key, url} -> {url_key, url} end)
+    |> IO.inspect
+    |> Enum.reduce(%{}, fn({url_key, url}, acc) -> Map.put(acc, url_key, url) end)
+    |> IO.inspect
   end
 
-  def upload(image_params) do
+  def upload(url_key, image_params) do
     # first check if user has uploaded an image
     unique_filename = get_unique_filename(image_params.filename)
 
     case File.read(image_params.path) do
       {:error, _} ->
-        {:error, "file could not be read"}
+        {:error, url_key, "file could not be read"}
       {:ok, image_binary} ->
         # returns image url string or error
-        put_object(unique_filename, image_binary)
+        res = put_object(unique_filename, image_binary)
+        Tuple.insert_at(res, 1, url_key)
+        # {:ok, url_key, url}
     end
   end
 
