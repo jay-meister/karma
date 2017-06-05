3422216b1ac1878a23282a22f185bfe1ca0ec485
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-06-05 11:04:44 +0100

    changes the scheme of the urls to https in production #267

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :karma, Karma.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "karmaradio.herokuapp.com", port: 443],
+  https: [port: {:system, "PORT"}],
+  url: [host: "karmaradio.herokuapp.com", port: 443, scheme: "https"],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
