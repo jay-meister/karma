ccd82f52f4138a61c64c6c2b094015ca28dec61d
Jack Murphy <jmurphy.web@gmail.com>
2017-06-06 21:04:53 +0100

    #291 - create merger module add prefix formatting function

--- web/controllers/helpers.ex
+++ web/controllers/helpers.ex
@@ -2,16 +2,6 @@ defmodule Karma.Controllers.Helpers do
 
   alias Karma.RedisCli
 
-  alias Karma.Repo
-
-  def get_data_for_merge(offer) do
-    %{user: Map.from_struct(Repo.get(Karma.User, offer.user_id)),
-      project: Map.from_struct(Repo.get(Karma.Project, offer.project_id)),
-      offer: Map.from_struct(Repo.get(Karma.Offer, offer.id)),
-      startpack: Map.from_struct(Repo.get_by(Karma.Startpack, user_id: offer.user_id))
-    }
-  end
-
   def user_startpack(user) do
     Ecto.assoc(user, :startpacks)
   end
