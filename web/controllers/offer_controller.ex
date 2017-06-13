dff8c7b705940120be111c8a86df7baeba4bdfcd
Jack Murphy <jmurphy.web@gmail.com>
2017-06-13 15:25:00 +0100

    #295 - remove error handling for updating document table

--- web/controllers/offer_controller.ex
+++ web/controllers/offer_controller.ex
@@ -291,20 +291,16 @@ defmodule Karma.OfferController do
                     |> put_flash(:error, msg)
                     |> redirect(to: project_offer_path(conn, :show, offer.project_id, offer))
                   {:ok, url} ->
-                    changeset =
-                      build_assoc(offer, :documents)
-                      |> Document.merged_url_changeset(%{url: url})
+                    # update documents table
+                    build_assoc(offer, :documents)
+                    |> Document.merged_url_changeset(%{url: url})
+                    |> Repo.insert()
 
-                    case Repo.insert(changeset) do
-                      {:ok, _document} ->
-                        conn
-                        |> put_flash(:info, "Document merged")
-                        |> redirect(to: project_offer_path(conn, :show, offer.project_id, offer))
-                      {:error, _changeset} ->
-                        conn
-                        |> put_flash(:error, "Error inserting merged url")
-                        |> redirect(to: project_offer_path(conn, :show, offer.project_id, offer))
-                    end
+                    # reply to user
+                    conn
+                    |> put_flash(:info, "Document merged")
+                    |> redirect(to: project_offer_path(conn, :show, offer.project_id, offer))
+
                 end
             end
         end
