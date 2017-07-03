40dbe1144869f25b13588186fc43c2654bd4e493
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-07-03 17:55:39 +0100

    displays the uploaded files in the startpack #371

--- web/models/startpack.ex
+++ web/models/startpack.ex
@@ -74,6 +74,8 @@ defmodule Karma.Startpack do
     field :box_rental_image, :any, virtual: true
     field :equipment_rental_image, :any, virtual: true
     field :vehicle_insurance_image, :any, virtual: true
+    field :vehicle_business_use_insurance_image, :any, virtual: true
+    field :vehicle_license_image, :any, virtual: true
     field :p45_image, :any, virtual: true
     field :schedule_d_letter_image, :any, virtual: true
     field :loan_out_company_cert_image, :any, virtual: true
@@ -375,5 +377,20 @@ defmodule Karma.Startpack do
       false ->
         struct
     end
+  end
+
+  def delete_changeset(struct, startpack) do
+    struct
+    |> cast(startpack, [
+      :box_rental_url,
+      :equipment_rental_url,
+      :schedule_d_letter_url,
+      :passport_url,
+      :vehicle_insurance_url,
+      :vehicle_license_url,
+      :vehicle_business_use_insurance_url,
+      :p45_url,
+      :loan_out_company_cert_url
+      ])
   end
 end
