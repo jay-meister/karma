a2e7b0c29ec3d78635894d997b874ebcc97cab07
Jack Murphy <jmurphy.web@gmail.com>
2017-07-04 17:22:30 +0100

    #429 - update altered_document controller to reflect what is reterned from sign.new_envelope

--- web/controllers/altered_documents_controller.ex
+++ web/controllers/altered_documents_controller.ex
@@ -8,8 +8,9 @@ defmodule Karma.AlteredDocumentController do
     # altered = Repo.get(AlteredDocument, id)
 
     altered_docs = Repo.all(from ad in AlteredDocument, where: ad.offer_id == ^o_id)
+
     case Sign.new_envelope(altered_docs, conn.assigns.current_user) do
-      {:ok, _altered_document} ->
+      {:ok, _msg} ->
         conn
         |> put_flash(:info, "Document sent to signees")
         |> redirect(to: project_offer_path(conn, :show, p_id, o_id))
