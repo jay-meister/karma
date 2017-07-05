6566049e68f3a39c48635f39ed1cd787e6426156
Jack Murphy <jmurphy.web@gmail.com>
2017-07-05 09:24:12 +0100

    #448 - add approver_type to signees

---
+++ priv/repo/migrations/20170705080045_add_approver_type_to_signees.exs
@@ -0,0 +1,9 @@
+defmodule Karma.Repo.Migrations.AddApproverTypeToSignees do
+  use Ecto.Migration
+
+  def change do
+    alter table(:signees) do
+      add :approver_type, :string
+    end
+  end
+end
