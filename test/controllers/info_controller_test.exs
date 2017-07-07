67b950d2dfa7af82e894b52fcd0bbbb4655905db
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-07-07 13:09:38 +0100

    adds privacy and terms placeholders #444

---
+++ test/controllers/info_controller_test.exs
@@ -0,0 +1,19 @@
+defmodule Karma.InfoControllerTest do
+  use Karma.ConnCase
+
+  setup do
+    user = insert_user() # This represents the user that created the project (PM)
+    conn = login_user(build_conn(), user)
+    {:ok, conn: conn, user: user}
+  end
+
+  test "/terms", %{conn: conn} do
+    conn = get conn, info_path(conn, :terms)
+    assert html_response(conn, 200) =~ "Terms and conditions"
+  end
+
+  test "/privacy", %{conn: conn} do
+    conn = get conn, info_path(conn, :privacy)
+    assert html_response(conn, 200) =~ "Privacy"
+  end
+end
