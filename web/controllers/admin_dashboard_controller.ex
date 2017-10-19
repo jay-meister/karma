872c3252f5e21de32cc4db7d68257333b77432c9
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-10-19 12:09:06 +0100

    adds new views for both custom fields and users in admin dashboard #650

--- web/controllers/admin_dashboard_controller.ex
+++ web/controllers/admin_dashboard_controller.ex
@@ -1,16 +1,26 @@
 defmodule Engine.AdminDashboardController do
   use Engine.Web, :controller
 
-  alias Engine.{Project}
+  alias Engine.{Project, User}
 
-  plug :authenticate_admin when action in [:index]
+  plug :authenticate_admin when action in [:index, :custom_fields, :users]
 
   def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+  def custom_fields(conn, _params) do
     all_projects =
       Repo.all(Project)
       |> Repo.preload(:custom_fields)
 
-    render conn, "index.html", all_projects: all_projects
+    render conn, "custom_fields.html", all_projects: all_projects
+  end
+
+  def users(conn, _params) do
+    all_users = Repo.all(User)
+
+    render conn, "users.html", all_users: all_users
   end
 
 end
